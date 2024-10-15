c953e145a08175bcbfca2b68c977863667b18cfd
Kyle Bomeisl <bomeisl@scarletmail.rutgers.edu>
2024-10-15 07:32:53 -0400

    Create plant model, migrate to local database

--- PlantDB/models.py
+++ PlantDB/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models import TextField
+from rest_framework.fields import IntegerField
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.TextField()
+    upperTemperature = models.IntegerField()
+    lowerTemperature = models.IntegerField()
+    upperHumidity = models.IntegerField()
+    lowerHumidity = models.IntegerField()
+    upperLight = models.TextField()
+    lowerLight = models.TextField()
