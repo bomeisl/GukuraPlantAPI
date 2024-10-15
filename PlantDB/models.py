1501339887d0f910a5931a087e8f99ed490c5805
Kyle Bomeisl <bomeisl@scarletmail.rutgers.edu>
2024-10-15 07:19:55 -0400

    Revert "created Plant model"
    This reverts commit 9067b8aded1eecbe794f7f639f8a72dddd8835f1.

--- PlantDB/models.py
+++ PlantDB/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
-class Plant(models.Model):
-    name = models.TextField()
-    upperTemperature = models.IntegerField()
-    lowerTemperature = models.IntegerField()
-    upperHumidity = models.IntegerField()
-    lowerHumidity = models.IntegerField()
-    upperLight = models.TextField()
-    lowerLight = models.TextField()
+# Create your models here.
