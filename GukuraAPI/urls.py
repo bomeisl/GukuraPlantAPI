1299ea64be20c4650fca5a80e7e88e5168b87e11
Kyle Bomeisl <bomeisl@scarletmail.rutgers.edu>
2024-10-16 10:58:23 -0400

    Map serialized json views to urls. Error catching and handling for bad requests.

--- GukuraAPI/urls.py
+++ GukuraAPI/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from PlantDB import views
+
+router = routers.DefaultRouter()
+router.register('plants', views.plant_list())
+router.register('plant', views.plant())
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, include(router.urls)),
 ]
--- PlantDB/views.py
+++ PlantDB/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import Group, User
+from django.template.context_processors import request
 from rest_framework import permissions, viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,19 +8,25 @@ from .models import Plant
 from .serializers import PlantSerializer
 
 @api_view(['GET'])
-def plant_list():
-    plants = Plant.objects.all()
-    serializer = PlantSerializer(plants, many=True)
-    return Response(serializer.data)
+def plant_list(request):
+    if request.method == 'GET':
+        plants = Plant.objects.all()
+        serializer = PlantSerializer(plants, many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def plant(request, name):
-    try:
-        plant = Plant.objects.get(name=name)
-        serializer = PlantSerializer(plant)
-        return Response(serializer.data)
-    except:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        try:
+            plant = Plant.objects.get(name=name)
+            serializer = PlantSerializer(plant)
+            return Response(serializer.data)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 
